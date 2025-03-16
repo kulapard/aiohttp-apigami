044d8653fa42339da67887c898d1022837ce87f4
Taras Drapalyuk <taras@drapalyuk.com>
2025-03-16 18:37:06 +0100

    refactor: update is_class_based_view to use more specific types

--- apispec_aiohttp/utils.py
+++ apispec_aiohttp/utils.py
@@ -1,8 +1,9 @@
 from inspect import isclass
 from string import Formatter
-from typing import Any
 
 from aiohttp import web
+from aiohttp.abc import AbstractView
+from aiohttp.typedefs import Handler
 
 
 def get_path(route: web.AbstractRoute) -> str | None:
@@ -16,7 +17,7 @@ def get_path_keys(path: str) -> list[str]:
     return [i[1] for i in Formatter().parse(path) if i[1]]
 
 
-def is_class_based_view(handler: Any) -> bool:
+def is_class_based_view(handler: Handler | AbstractView) -> bool:
     if not isclass(handler):
         return False
 
