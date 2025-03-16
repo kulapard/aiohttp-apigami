758a3cc4a5400b6c8ff060485e1088960b427170
Taras Drapalyuk <taras@drapalyuk.com>
2025-03-16 19:02:00 +0100

    update aiohttp version and improve type hint (#13)
    update aiohttp version and improve type hint for is_class_based_view

--- apispec_aiohttp/utils.py
+++ apispec_aiohttp/utils.py
@@ -9,15 +9,14 @@ from aiohttp.typedefs import Handler
 def get_path(route: web.AbstractRoute) -> str | None:
     if route.resource is None:
         return None
-    path_info = route.resource.get_info()
-    return path_info.get("path") or path_info.get("formatter")
+    return route.resource.canonical
 
 
 def get_path_keys(path: str) -> list[str]:
     return [i[1] for i in Formatter().parse(path) if i[1]]
 
 
-def is_class_based_view(handler: Handler | AbstractView) -> bool:
+def is_class_based_view(handler: Handler | type[AbstractView]) -> bool:
     if not isclass(handler):
         return False
 
