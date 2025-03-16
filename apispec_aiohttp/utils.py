9f054d46b46bf4616e9e56993cf80516a4bc7b50
Taras Drapalyuk <taras@drapalyuk.com>
2025-03-16 18:33:23 +0100

    refactor: replace issubclass_py37fix with is_class_based_view for clarity

--- apispec_aiohttp/utils.py
+++ apispec_aiohttp/utils.py
@@ -1,3 +1,4 @@
+from inspect import isclass
 from string import Formatter
 from typing import Any
 
@@ -15,8 +16,8 @@ def get_path_keys(path: str) -> list[str]:
     return [i[1] for i in Formatter().parse(path) if i[1]]
 
 
-def issubclass_py37fix(cls: Any, cls_info: Any) -> bool:
-    try:
-        return issubclass(cls, cls_info)
-    except TypeError:
+def is_class_based_view(handler: Any) -> bool:
+    if not isclass(handler):
         return False
+
+    return issubclass(handler, web.View)
