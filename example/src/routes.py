1c704e05fe805d5902f55f5b504e6fac8f5969a3
Taras Drapalyuk <taras@drapalyuk.com>
2025-03-16 17:27:13 +0100

    add class-based view to example app (#11)

--- example/src/routes.py
+++ example/src/routes.py
@@ -1,10 +1,12 @@
 # routes.py
 from aiohttp import web
 
-from .views import create_user, get_user, get_users
+from .views import UserView, create_user, get_users
 
 
 def setup_routes(app: web.Application) -> None:
+    # function-based routes
     app.router.add_post("/users", create_user)
     app.router.add_get("/users", get_users)
-    app.router.add_get("/users/{id}", get_user)
+    # class-based routes
+    app.router.add_view("/users/{id}", UserView)
