4402a6825c35748c46465419a99f1b402acab9d7
Taras Drapalyuk <taras@drapalyuk.com>
2025-03-23 13:01:59 +0100

    feat: introduce RouteData dataclass and refactor route registration logic for improved clarity

--- apispec_aiohttp/route_processor.py
+++ apispec_aiohttp/route_processor.py
@@ -1,3 +1,6 @@
+from collections.abc import Iterator
+from dataclasses import dataclass
+
 from aiohttp import web
 from aiohttp.hdrs import METH_ALL
 
@@ -6,6 +9,13 @@ from .processors import create_processor
 from .spec import SpecManager
 from .typedefs import HandlerType
 from .utils import get_path, is_class_based_view
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class RouteData:
+    method: str
+    path: str
+    handler: HandlerType
 
 
 class RouteProcessor:
@@ -18,37 +28,44 @@ class RouteProcessor:
         self._prefix = prefix
         self._processor = create_processor(spec_manager)
 
-    def register_routes(self, app: web.Application) -> None:
-        """Register all routes from the application."""
+    @staticmethod
+    def _get_implemented_methods(class_based_view: HandlerType) -> Iterator[tuple[str, HandlerType]]:
+        for m in METH_ALL:
+            method_name = m.lower()
+            if hasattr(class_based_view, method_name):
+                yield method_name, getattr(class_based_view, method_name)
+
+    def _iter_routes(self, app: web.Application) -> Iterator[RouteData]:
         for route in app.router.routes():
             # Class based views have multiple methods
-            # Register each method separately
             if is_class_based_view(route.handler):
-                for attr in dir(route.handler):
-                    if attr.upper() in METH_ALL:
-                        method = attr
-                        sub_handler = getattr(route.handler, attr)
-                        self.register_route(route=route, method=method, handler=sub_handler)
+                for method_name, method_func in self._get_implemented_methods(route.handler):
+                    path = get_path(route)
+                    if path is not None:
+                        yield RouteData(method=method_name, path=path, handler=method_func)
 
             # Function based views have a single method
             else:
-                method = route.method.lower()
-                handler = route.handler
-                self.register_route(route=route, method=method, handler=handler)
+                path = get_path(route)
+                if path is not None:
+                    method = route.method.lower()
+                    handler = route.handler
+                    yield RouteData(method=method, path=path, handler=handler)
 
-    def register_route(self, *, route: web.AbstractRoute, method: str, handler: HandlerType) -> None:
+    def register_routes(self, app: web.Application) -> None:
+        """Register all routes from the application."""
+        for route in self._iter_routes(app):
+            self.register_route(route)
+
+    def register_route(self, route: RouteData) -> None:
         """Register a single route."""
-        if not hasattr(handler, API_SPEC_ATTR):
+        if not hasattr(route.handler, API_SPEC_ATTR):
             return None
 
-        url_path = get_path(route)
-        if not url_path:
-            return None
-
-        handler_apispec = getattr(handler, API_SPEC_ATTR)
-        full_path = self._prefix + url_path
+        handler_apispec = getattr(route.handler, API_SPEC_ATTR)
+        full_path = self._prefix + route.path
         handler_apispec = self._processor.get_path_operations(
-            path=full_path, method=method, handler_apispec=handler_apispec
+            path=full_path, method=route.method, handler_apispec=handler_apispec
         )
         if handler_apispec is not None:
-            self._spec_manager.add_path(path=full_path, method=method, handler_apispec=handler_apispec)
+            self._spec_manager.add_path(path=full_path, method=route.method, handler_apispec=handler_apispec)
